023ff8f1408c0359d20a70ef55642e036aa4728f
chuang] <33791604+chuang861012@users.noreply.github.com>
2018-10-28 15:36:42 +0800

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+# simple scraper for everydayhealth (早安健康)
+## Requirements
+- requests
+- lxml
+## Usage
+Enter a Keyword. The program automatically scrape through all the articles.    
+You will receive a list of all the results in dict.    
+A single dict would be in this structure : 
+```
+{
+    title:<str>,
+    date:<str>,
+    author:<str>,
+    body:<str>
+}
+```
+## Demo
+![image](https://github.com/chuang861012/exhentai-huolu-chrome_extension/blob/master/demo.gif)
+## Todo
+A few the articles in this website are in different html structure. The program won't get the result correctly. But it won't crash. Must be fixed in the future.
---
+++ app.py
@@ -0,0 +1,46 @@
+import requests
+from lxml import etree
+from time import sleep
+
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"}
+
+def getSearchResult(url,results=[]):
+    print('scraping:',url)
+    res = requests.get(url,headers=headers)
+    content = res.content.decode()
+    html = etree.HTML(content)
+    links = html.xpath('//*[@id="container"]/div[1]/div[2]/main/div[2]/div/div/div/h3/a/@href')
+    
+    for link in links:
+        results.append(getArticleContent(f'https://www.everydayhealth.com.tw{link}'))
+        
+    # goto next page
+    next_page = html.xpath('//div[@class="pager"]/ul/li/a[text()=">"]/@href')
+    if len(next_page) > 0:
+        # delay 0.5 sec
+        sleep(.5)
+        return getSearchResult(next_page[0])
+    else:
+        return results
+
+def getArticleContent(url):
+    res = requests.get(url,headers=headers)
+    content = res.content.decode()
+    html = etree.HTML(content)
+    
+    title = html.xpath('//h1[@itemprop="headline"]/text()')
+    date = html.xpath('//span[@itemprop="datePublished"]/text()')
+    author = html.xpath('//span[@itemprop="author"]//text()')
+    body = html.xpath('//div[@id="article_page"]//text()')
+    
+    return {
+        'title':''.join(title),
+        'date':''.join(date),
+        'author':''.join(author),
+        'body':''.join(body)
+    }
+
+if __name__ == "__main__":
+    keyword = input('keyword:')
+    data = getSearchResult(f'https://www.everydayhealth.com.tw/tag/{keyword}/1')
+    print(data)
